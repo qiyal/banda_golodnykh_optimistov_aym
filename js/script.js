045bc5e234fa81212267f17b015cbd9c9d8b910d
Yerbolat Pazyl <twinspazyl@gmail.com>
2020-05-12 04:03:16 +0600

    commit 2. Add contact page

--- js/script.js
+++ js/script.js
@@ -6,8 +6,10 @@ btnLogIn.addEventListener('click', function () {
     const modal = `
         <div class="modal">
             <div class="modal-body">
-                <button class="btn-modal-close">X</button>
-                <h1 class="title-login">Log In</h1>
+                <div class="modal-head">
+                    <h1 class="title-login">Log In</h1>
+                    <button class="btn-modal-close">X</button>
+                </div>
                 <div class="form-auth">
                     <form action="">
                         <input class="modal-input" type="text" name="login" placeholder="Email">
@@ -19,6 +21,7 @@ btnLogIn.addEventListener('click', function () {
         </div>
     `;
 
+    document.querySelector('body').style.overflow = 'hidden';
     document.querySelector('body').insertAdjacentHTML('beforeEnd', modal);
 
     const modalCloseBtn = document.querySelector('.btn-modal-close');
@@ -27,11 +30,11 @@ btnLogIn.addEventListener('click', function () {
     function closeModal() {
         console.log("close");
         document.querySelector('body').removeChild(document.querySelector('.modal'));
+        document.querySelector('body').style.overflow = '';
     }
 
     function submitModal(event) {
         event.preventDefault();
-        localStorage.setItem('modal', 1);
     }
 
     modalCloseBtn.addEventListener('click', closeModal);
