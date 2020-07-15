e298a57c8456418a7be9389171648cd3ea3c8789
Yerbolat <twinspazyl@gmail.com>
2020-07-15 23:05:35 +0600

    did there changes last day, I do not remember)

--- js/parallax.js
+++ js/parallax.js
@@ -1,5 +1,5 @@
 const paralaxBg = document.querySelector('.paralax-box');
-function parallaxEffect(event) {
+function parallaxEffect() {
     if(pageYOffset > 1353 && pageYOffset < 3044) {
         const position = '-340px ' + (-665 + (pageYOffset  / 5)) + 'px';
         paralaxBg.style.backgroundPosition = position;
